54ec1466796f7b329a3d2cd42aed95d0e1393e79
Emel-D-Medina <emeldmedina@unicesar.edu.co>
2024-04-29 11:32:00 -0500

    Archivos Primarios

---
+++ Condor Viajero/Condor Viajero/Frm_Mapa.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Condor_Viajero
+{
+    partial class Frm_Mapa
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Mapa = new GMap.NET.WindowsForms.GMapControl();
+            this.SuspendLayout();
+            // 
+            // Mapa
+            // 
+            this.Mapa.Bearing = 0F;
+            this.Mapa.CanDragMap = true;
+            this.Mapa.EmptyTileColor = System.Drawing.Color.Navy;
+            this.Mapa.GrayScaleMode = false;
+            this.Mapa.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.Mapa.LevelsKeepInMemory = 5;
+            this.Mapa.Location = new System.Drawing.Point(12, 12);
+            this.Mapa.MarkersEnabled = true;
+            this.Mapa.MaxZoom = 2;
+            this.Mapa.MinZoom = 2;
+            this.Mapa.MouseWheelZoomEnabled = true;
+            this.Mapa.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.Mapa.Name = "Mapa";
+            this.Mapa.NegativeMode = false;
+            this.Mapa.PolygonsEnabled = true;
+            this.Mapa.RetryLoadTile = 0;
+            this.Mapa.RoutesEnabled = true;
+            this.Mapa.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.Mapa.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.Mapa.ShowTileGridLines = false;
+            this.Mapa.Size = new System.Drawing.Size(776, 426);
+            this.Mapa.TabIndex = 0;
+            this.Mapa.Zoom = 0D;
+            // 
+            // Frm_Mapa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Mapa);
+            this.Name = "Frm_Mapa";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl Mapa;
+    }
+}
+
---
+++ Condor Viajero/Condor Viajero/Frm_Mapa.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using GMap.NET;
+using GMap.NET.CacheProviders;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using GMap.NET.WindowsForms.ToolTips;
+
+namespace Condor_Viajero
+{
+    public partial class Frm_Mapa : Form
+    {
+        PointLatLng Valledupar = new PointLatLng(10.469542991069783, -73.25712765309014);
+
+        public Frm_Mapa()
+        {
+            InitializeComponent();
+            Mapa.MapProvider = OpenStreetMapProvider.Instance;
+            GMaps.Instance.Mode = AccessMode.ServerAndCache;
+            BingMapProvider.Instance.ClientKey = Cls_ApiKey.BingMapApiKey;
+            Mapa.Position = Valledupar;
+            Mapa.DragButton = MouseButtons.Left;
+            Mapa.MinZoom = 12; Mapa.MaxZoom = 18; Mapa.Zoom = 13;
+        }
+    }
+}
