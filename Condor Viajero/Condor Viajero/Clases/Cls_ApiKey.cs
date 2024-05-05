62f756702f271e8444da6f3ed03a848c4107b3f1
Emel-D-Medina <emeldmedina@unicesar.edu.co>
2024-05-04 23:37:10 -0500

    Nuevo Repositorio
    Un nuevo repositorio para corregir los anteriores errores presentados en el anterior

---
+++ Condor Viajero/Condor Viajero/Clases/Cls_ApiKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condor_Viajero.Clases
+{
+    public static class Cls_ApiKey
+    {
+        public static string BingMapApiKey { get; } = @"ApUTaO7sHvGZqA6QHbqWc3xCLF7i6InJfoCThewaS9kbjCASLhy2gCnU2QvNI2SA";
+    }
+}
